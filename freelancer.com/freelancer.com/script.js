7cc28cc28db2a383d83e5e633f9361d7ace99218
pliki <doujavajs@gmail.com>
2019-07-28 18:26:27 +0200

    home

---
+++ freelancer.com/freelancer.com/script.js
@@ -0,0 +1,31 @@
+let activeElement = 0;
+const timeChange = 4000;
+
+const colorImgHtml = document.querySelector('.color');
+const grayImgHtml = document.querySelector('.gray');
+const h1Html = document.querySelector('.member h1');
+const h2Html = document.querySelector('.member h2');
+
+
+const colorImages = ['img/s1.png', 'img/s2.png'];
+const grayImages = ['img/s1a.png', 'img/s2a.png'];
+const names = ['Liwiusz Piórko', 'Hubert Gościniak'];
+const professions = ['Programista Java, JS', 'Programista Java, JS'];
+
+function changeElement() {
+  activeElement++;
+  if (activeElement == colorImages.length) {
+    activeElement = 0;
+  }
+  colorImgHtml.src = colorImages[activeElement];
+  grayImgHtml.src = grayImages[activeElement];
+  h1Html.textContent = names[activeElement];
+  h2Html.textContent = professions[activeElement];
+
+}
+
+setInterval(changeElement, timeChange)
+
+$(".burger").on("click", function () {
+  $(".fas, nav").toggleClass("off");
+})
